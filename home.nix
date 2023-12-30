65f96b27593224d6a5e3cebc8120186ea8a396ce
Avraham Ben Aram <avrahambenaramsouza@gmail.com>
2023-12-30 16:25:16 -0300

    feat: power-menu and waybar configuration

--- home.nix
+++ home.nix
@@ -1,5 +1,8 @@
 { inputs, config, pkgs, ... }:
 
+let
+  power-menu = import ./scripts/power-menu.nix { inherit pkgs; };
+in
 {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
@@ -35,6 +38,9 @@
 
     pkgs.nwg-look
 	pkgs.swaylock-effects
+
+	# Scripts
+	power-menu
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -145,11 +151,219 @@
 	'';
   };
 
+  programs.waybar = {
+    enable = true;
+	settings = {
+	  mainBar = {
+	    layer = "top";
+        position = "top";
+
+        # If height property would be not present, it'd be calculated dynamically
+        height = 24;
+	    margin-left = 15;
+	    margin-right = 15;
+	    margin-top = 15;
+	
+        modules-left = [
+            "custom/launcher"
+		    "hyprland/workspaces"
+        ];
+ 
+        modules-center = [
+		    "clock"
+    	    "hyprland/window"
+		    "tray"
+        ];
+
+        modules-right = [
+            "network"
+            "memory"
+            "cpu"
+            "pulseaudio"
+            "battery"
+            #"custom/PBPbattery"
+            "backlight#icon"
+            "backlight#value"
+            "custom/weather"
+            "custom/power"
+        ];
+
+        # Modules
+
+        "hyprland/window" = {
+            max-length = 200;
+            separate-outputs = true;
+        };
+
+        "idle_inhibitor" = {
+            format = "{icon} ";
+            format-icons ={
+                activated = "";
+                deactivated = "";
+            };
+        };
+
+        battery = {
+            states = {
+                # good = 95;
+                warning = 30;
+                critical = 15;
+            };
+            format = "{capacity}% {icon}";
+            format-charging = "{capacity}% ";
+            format-plugged = "{capacity}% ";
+            # format-good = ""; # An empty format will hide the module
+            # format-full = "";
+            format-icons = ["" "" "" "" ""];
+        };
+
+        "custom/PBPbattery" = {
+            exec = "~/.config/waybar/scripts/PBPbattery.sh";
+            format = "{}";
+        };
+
+        clock = {
+            interval = 10;
+            format-alt = " {:%e %b %Y}"; # Icon: calendar-alt
+            format = "{:%H:%M}";
+            tooltip-format = "{:%e %B %Y}";
+        };
+
+        cpu = {
+            interval = 5;
+            format = "    {usage}% ({load})"; # Icon: microchip
+            states = {
+                warning = 70;
+                critical = 90;
+            };
+            on-click = "alacritty -e 'btop'";
+        };
+
+        memory = {
+            interval = 5;
+            format = "   {}%"; # Icon: memory
+            on-click = "alacritty -e 'btop'"; 
+            states = {
+                warning = 70;
+                critical = 90;
+            };
+        };
+
+        network = {
+            interval = 5;
+            format-wifi = "   ({signalStrength}%)"; # Icon: wifi
+            format-ethernet = "   {ifname}: {ipaddr}/{cidr}"; # Icon: ethernet
+            format-disconnected = "⚠   Disconnected";
+            tooltip-format = "{essid}: {ifname} / {ipaddr}";
+            on-click = "alacritty -e 'nmtui'";
+        };
+
+        "network#vpn" = {
+            interface = "tun0";
+            format = "  {essid} ({signalStrength}%)";
+            format-disconnected = "⚠  Disconnected";
+            tooltip-format = "{ifname}: {ipaddr}/{cidr}";
+        };
+    
+        "hyprland/workspaces" = {
+	      on-scroll-up = "hyprctl dispatch workspace e+1";
+	      on-scroll-down = "hyprctl dispatch workspace e-1";
+            format = " {icon} ";
+            format-icons = {
+                "1" = "";
+                "2" = "";
+                "3" = "";
+                "4" = "";
+          };
+        };
+
+        pulseaudio = {
+            scroll-step = 1; # %; can be a float
+            format = "{volume}% {icon}";
+            format-bluetooth = " {volume}%";
+            format-bluetooth-muted = " ";
+            format-muted = " {format_source}";
+            format-source = "{volume}% ";
+            format-source-muted = "";
+            format-icons = {
+                headphone = "";
+                hands-free = "וֹ";
+                headset = "  ";
+                phone = "";
+                portable = "";
+                car = "";
+                default = [""];
+            };
+            on-click = "pavucontrol";
+            on-scroll-up = "pactl set-sink-volume @DEFAULT_SINK@ +2%";
+            on-scroll-down = "pactl set-sink-volume @DEFAULT_SINK@ -2%";
+        };
+
+        # to use the weather module replace <your_location> with your city or town
+        # note: do not use spaces: new york would be newyork
+        "custom/weather" = {
+            exec = "~/.config/waybar/scripts/weather.sh MogiDasCruzes"; 
+            return-type = "json";
+            interval = 600;
+        };
+
+        tray = {
+            icon-size = 18;
+            spacing = 10;
+         };
+
+        "backlight#icon" = {
+            format = "{icon}";
+            format-icons = [""];
+            on-scroll-down = "brightnessctl -c backlight set 1%-";
+            on-scroll-up = "brightnessctl -c backlight set +1%";
+        };
+
+        "backlight#value" = {
+             format = "{percent}%";
+             on-scroll-down = "brightnessctl -c backlight set 1%-";
+             on-scroll-up = "brightnessctl -c backlight set +1%";
+        };
+
+        "custom/firefox" = {
+            format = " ";
+            on-click = "exec firefox";
+            tooltip = false;
+        };
+
+        "custom/terminal" = {
+            format = " ";
+            on-click = "exec alacritty";
+            tooltip = false;
+        };
+
+        "custom/files" = {
+            format = " ";
+            on-click = "exec thunar";
+            tooltip = false;
+        };
+
+        "custom/launcher" = {
+            format =" ";
+            on-click = "exec wofi -c ~/.config/wofi/config -I";
+            tooltip = false;
+        };
+
+        "custom/power" = {
+            format ="⏻";
+            on-click = "exec power-menu";
+            tooltip = false;
+        };
+	  };
+
+	};
+  };
+ 
   programs.zathura = {
     enable = true;
 	extraConfig = ''
 	  unmap f
-	'';
+	''; 
 	mappings = {
 	  r = "reload";
 	  p = "print";
@@ -160,43 +374,47 @@
 	options = {
 	  "selection-clipboard" = "clipboard";
 	  font = "Fira Code";
-
+ 
 	  # Open document in fit-width mode by default
 	  "adjust-open" = "best-fit";
-
+ 
 	  # search settings
 	  "incremental-search" = true;
-
-	  # padding
+ 
+	  # padding 
 	  "statusbar-h-padding" = 0;
 	  "statusbar-v-padding" = 0;
-	};
-  };
-
+	}; 
+  }; 
+ 
   programs.zsh = {
     enable = true;
 	initExtra = "pfetch";
-  };
+  }; 
 
   services.spotifyd = {
     enable = true;
 	settings = import ./.spotifyd.nix;
-  };
+  }; 
 
   wayland.windowManager.hyprland = {
     enable = true;
-  };
-
-  gtk = {
+  }; 
+ 
+  gtk = { 
 	  enable = true;
 	  theme = {
 		package = pkgs.lounge-gtk-theme;
 		name = "Lounge-night";
-	  };
-
-	  iconTheme = {
+	  }; 
+ 
+	  iconTheme = { 
 		package = pkgs.gnome.adwaita-icon-theme;
 		name = "Adwaita";
-	  };
-  };
-}
+	  }; 
+  }; 
+} 
+
+
+
+
