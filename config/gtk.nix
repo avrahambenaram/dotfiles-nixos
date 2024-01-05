3796802a38a3dfb5317fd7d0db16e7945ed24f08
Avraham Ben Aram <avrahambenaramsouza@gmail.com>
2024-01-05 12:43:48 -0300

    feat: mimeapps

--- config/gtk.nix
+++ config/gtk.nix
@@ -13,4 +13,15 @@
         name = "Adwaita";
       }; 
   }; 
+
+  xdg.mimeApps = {
+    enable = true;
+	defaultApplications = {
+	  "text/html" = "vivaldi-stable.desktop";
+	  "x-scheme-handler/http" = "vivaldi-stable.desktop";
+	  "x-scheme-handler/https" = "vivaldi-stable.desktop";
+	  "x-scheme-handler/about" = "vivaldi-stable.desktop";
+	  "x-scheme-handler/unknown" = "vivaldi-stable.desktop";
+	};
+  };
 }
