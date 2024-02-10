9a8587035e4e14edda5c029a37adf48bbd11a994
Avraham Ben Aram <avrahambenaramsouza@gmail.com>
2024-02-10 02:30:17 -0300

    ch: dap in lua

--- config/nvim/dap.nix
+++ config/nvim/dap.nix
@@ -1,16 +1,16 @@
-{
-  imports = [
-    ./dap/node.nix
-  ];
+{ config, pkgs, ... }:
 
+let
+  dapJsServer = builtins.fetchTarball {
+    url = "https://github.com/microsoft/vscode-js-debug/releases/download/v1.86.1/js-debug-dap-v1.86.1.tar.gz";
+    name = "vscode-js-debug";
+  };
+in
+{
   programs.nixvim.plugins.dap = {
     enable = true;
     signs.dapBreakpoint.text = "🛑";
   };
-  programs.nixvim.extraConfigLua = ''
-  vim.g.dap_virtual_text = true
-  vim.g.dap_virtual_text_show_scopes = true
-  '';
   programs.nixvim.keymaps = [
     {
       mode = ["n"];
@@ -73,4 +73,51 @@
       options.silent = true;
     }
   ];
+
+  programs.nixvim.extraConfigLua = ''
+  vim.g.dap_virtual_text = true
+  vim.g.dap_virtual_text_show_scopes = true
+
+  local dap = require('dap')
+
+  dap.adapters = {
+    ["pwa-node"] = {
+      ["host"] = "localhost",
+      ["port"] = "${"$"}{port}",
+      ["type"] = "server",
+      ["executable"] = {
+        command = "node",
+        -- 💀 Make sure to update this path to point to your installation
+        args = {"${dapJsServer}/src/dapDebugServer.js", "${"$"}{port}"},
+      }
+    },
+    ["coreclr"] = {
+      type = 'executable',
+      command = '${pkgs.netcoredbg}/bin/netcoredbg',
+      args = {'--interpreter=vscode'}
+    }
+  }
+
+  dap.configurations = {
+    ["javascript"] = {
+      {
+        ["cwd"] = "${"$"}{workspaceFolder}",
+        ["name"] = "Launch file",
+        ["program"] = "${"$"}{file}",
+        ["request"] = "launch",
+        ["type"] = "pwa-node"
+      }
+    },
+    ["cs"] = {
+      {
+        type = "coreclr",
+        name = "launch - netcoredbg",
+        request = "launch",
+        program = function()
+            return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+        end,
+      }
+    }
+  }
+  '';
 }
