6fd425992328a610be3d838cf85c14e76b9391d7
Avraham Ben Aram <avrahambenaramsouza@gmail.com>
2024-02-18 18:43:49 -0300

    feat(nixvim): add keybind to neotest run and setup jest

--- config/nvim/neotest.nix
+++ config/nvim/neotest.nix
@@ -3,13 +3,21 @@
 {
   programs.nixvim.extraPlugins = with pkgs.vimPlugins; [
     neotest
-    neotest-vitest
+    neotest-jest
     neotest-dotnet
   ];
   programs.nixvim.extraConfigLua = ''
 require("neotest").setup({
   adapters = {
-    require("neotest-dotnet")
+    require("neotest-dotnet"),
+    require('neotest-jest')({
+      jestCommand = "npm test --",
+      jestConfigFile = "jest.config.ts",
+      env = { CI = true },
+      cwd = function(path)
+        return vim.fn.getcwd()
+      end,
+    }),
   }
 })
   '';
@@ -17,6 +25,11 @@ require("neotest").setup({
     {
       key = "<leader>dn";
       action = ":lua require('neotest').run.run({strategy = 'dap'})<CR>";
+      options.silent = true;
+    }
+    {
+      key = "<leader>dr";
+      action = ":lua require('neotest').run.run({ vim.fn.expand('%') })<CR>";
       options.silent = true;
     }
   ];
