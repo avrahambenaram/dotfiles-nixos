29f071416ade5bdfb730234b5aa2f9e2bbb426f3
Avraham Ben Aram <avrahambenaramsouza@gmail.com>
2024-02-24 21:16:49 -0300

    chore(nixvim): move tabnine to nix

--- config/nvim/tabnine.nix
+++ config/nvim/tabnine.nix
@@ -1,23 +1,21 @@
 { config, pkgs, ... }:
 
 {
-  programs.nixvim.extraPlugins = [pkgs.vimPlugins.cmp-tabnine];
-  programs.nixvim.extraConfigLua = ''
-local tabnine = require('cmp_tabnine.config')
-
-tabnine:setup({
-	max_lines = 1000,
-	max_num_results = 20,
-	sort = true,
-	run_on_every_keystroke = true,
-	snippet_placeholder = '..',
-	ignored_file_types = {
-		-- default is not to ignore
-		-- uncomment to ignore in lua:
-		-- lua = true
-	},
-	show_prediction_strength = false,
-	min_percent = 0
-})
-  '';
+  programs.nixvim.plugins.cmp-tabnine = {
+    enable = true;
+    extraOptions = {
+      max_lines = 1000;
+      max_num_results = 20;
+      sort = true;
+      run_on_every_keystroke = true;
+      snippet_placeholder = "..";
+      ignored_file_types = {
+        # default is not to ignore
+        # uncomment to ignore in lua:
+        # lua = true
+      };
+      show_prediction_strength = false;
+      min_percent = 0;
+    };
+  };
 }
