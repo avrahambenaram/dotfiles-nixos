f6bb9e756a062374c4bc6044897f7fd2eedb3ea5
Avraham Ben Aram <avrahambenaramsouza@gmail.com>
2024-11-20 16:29:18 -0300

    fix(nixvim): treesitter now configured

--- config/nvim/treesitter.nix
+++ config/nvim/treesitter.nix
@@ -1,4 +1,11 @@
 {
-  programs.nixvim.plugins.treesitter.enable = true;
+  programs.nixvim.plugins.treesitter = {
+    enable = true;
+    settings = {
+      auto_install = true;
+      highlight.enable = true;
+      indent.enable = true;
+    };
+  };
   programs.nixvim.plugins.rainbow-delimiters.enable = true;
 }
