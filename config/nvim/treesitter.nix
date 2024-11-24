79c710509e16b8bcf8201a403a9f866f4a2cf04e
Avraham Ben Aram <avrahambenaramsouza@gmail.com>
2024-11-24 13:48:08 -0300

    fix(nixvim): treesitter indent trouble and disable highlight for html

--- config/nvim/treesitter.nix
+++ config/nvim/treesitter.nix
@@ -1,10 +1,14 @@
+{ pkgs, ... }:
+
 {
   programs.nixvim.plugins.treesitter = {
     enable = true;
     settings = {
       auto_install = true;
-      highlight.enable = true;
-      indent.enable = true;
+      highlight = {
+        enable = true;
+        disable = ["html"];
+      };
     };
   };
   programs.nixvim.plugins.rainbow-delimiters.enable = true;
